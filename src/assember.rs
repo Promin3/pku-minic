405d3bdbece2ecc7bf61176358ad852f17c00b3b
Promin <3056063115@qq.com>
2025-06-08 17:40:48 +0800

    lv3.1

--- src/assember.rs
+++ src/assember.rs
@@ -12,27 +12,72 @@ impl GenerateAsm for koopa::ir::Program {
   }
 }
 
-use koopa::ir::ValueKind;
+use koopa::ir::{BinaryOp, ValueKind};
 impl GenerateAsm for koopa::ir::FunctionData {
     fn generate(&self, output: &mut String) {
         output.push_str(format!("  .globl {}\n",self.name().trim_start_matches('@')).as_str());
         output.push_str(format!("{}:\n",self.name().trim_start_matches('@')).as_str());
         for (&_bb, node) in self.layout().bbs() {
+
+            let mut t_register = 0;
+
             for &inst in node.insts().keys(){
                 let inst_value_data = self.dfg().value(inst);
+                
                 match inst_value_data.kind(){
-                    ValueKind:: Return(ret) =>{
+                    ValueKind::Binary(bin) =>{
+                        let op = bin.op();
+                        let lhs_value_id = bin.lhs();
+                        let lhs_value_data = self.dfg().value(lhs_value_id);
+                        let rhs_value_id = bin.rhs();
+                        let rhs_value_data = self.dfg().value(rhs_value_id);
+                        match op {
+                            BinaryOp::Eq =>{
+                                match lhs_value_data.kind() {
+                                    ValueKind::Integer(int) => {
+                                        output.push_str(&format!("  li t{}, {}\n",t_register,int.value()));
+                                    }
+                                    ValueKind::Binary(_bin) => {
+                                        output.push_str(&format!("  li t{}, t{}\n",t_register,t_register-1));
+                                    }
+                                    _ => unreachable!()
+                                    
+                                };
+                                output.push_str(&format!("  xor t{}, t{}, x0\n",t_register,t_register));
+                                output.push_str(&format!("  seqz t{}, t{}\n",t_register,t_register));
+                                t_register += 1;
+                            },
+                            BinaryOp::Sub =>{
+                                match rhs_value_data.kind() {
+                                    ValueKind::Integer(int) => {
+                                        output.push_str(&format!("  sub t{}, x0, {}\n",t_register,int.value()));
+                                    }
+                                    ValueKind::Binary(_bin) => {
+                                        output.push_str(&format!("  sub t{}, x0, t{}\n",t_register,t_register-1));
+                                    }
+                                    _ => unreachable!()
+                                }
+                                t_register += 1;
+                            },
+                            _ => unreachable!()
+                        }
+                    },
+                
+                    ValueKind::Return(ret) =>{
                         let ret_value_id = ret.value().unwrap();
                         let ret_value_data = self.dfg().value(ret_value_id);
                         match ret_value_data.kind(){
                             ValueKind::Integer(int) =>{
                                 output.push_str(&format!("  li a0, {}\n",int.value()));
                             }
+                            ValueKind::Binary(_bin) =>{
+                                output.push_str(&format!("  mv a0, t{}\n",t_register-1));
+                            }
                             _=> unreachable!()
                         }
 
                         output.push_str("  ret");
-                    }
+                    },
                     _ => unreachable!() 
                 }
 
@@ -40,5 +85,4 @@ impl GenerateAsm for koopa::ir::FunctionData {
         }
     }
 }
-
 
--- src/main.rs
+++ src/main.rs
@@ -19,22 +19,37 @@ fn main() -> Result<()> {
   // 解析命令行参数
   let mut args = args();
   args.next();
-  let _mode = args.next().unwrap();
+  let mode = args.next().unwrap();
   let input = args.next().unwrap();
   args.next();
   let output = args.next().unwrap();
 
-  // 读取输入文件
   let input = read_to_string(input)?;
 
   // 调用 lalrpop 生成的 parser 解析输入文件
   let ast = sysy::CompUnitParser::new().parse(&input).unwrap();
+
+  // 将 AST 转换为 Koopa IR
   let ir_string = generate_ir(&ast);
   println!("{}", ir_string);
-  let driver = koopa::front::Driver::from(ir_string);
+
+  // 将 文本形式的 Koopa IR 转换为内存形式
+  let ir_string_copy = ir_string.clone();
+  let driver = koopa::front::Driver::from(ir_string_copy);
   let program = driver.generate_program().unwrap();
+
+  // 将 内存形式Koopa IR 转换为 RISC-V 汇编代码
   let mut asm_string = String::new();
   program.generate(&mut asm_string);
-  fs::write(output, asm_string)?;
+  println!("{}", asm_string);
+
+  if mode == "-koopa" {
+    // 如果是 -koopa 模式，输出 Koopa IR
+    fs::write(output, ir_string)?;
+  } else if mode == "-riscv" {
+    // 如果是 -riscv 模式，输出 RISC-V 汇编代码
+    fs::write(output, asm_string)?;
+  }
+
   Ok(())
 }
